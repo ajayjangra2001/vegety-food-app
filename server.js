d7bb4172c4d1f918b3e7a399ec975b972dbb2362
Ajay <112325784+ajayjangra2001@users.noreply.github.com>
2022-10-02 18:17:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,27 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const connection = require('./Config/db');
 const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
-
-// const destination = path.join(path.resolve(__dirname, '..'), 'Uploads')
 
 
 (async () => await connection())();
 
 app.use(cors());
+app.use(express.static('./public/build'));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(bodyParser.urlencoded({
-//     extended: false
-// }));
-// app.use(express.static(destination));
-
-app.listen(5000, () => { 
+const port = process.env.PORT || 5000;
+app.listen(port, () => { 
     console.log('server has been started'); 
 });
 
@@ -37,5 +31,4 @@ app.use('/admin', adminRouter);
 app.use('/plan', planRouter);
 app.use('/review', reviewRouter);
 app.use('/planDetails/review', reviewRouter);
-
 
